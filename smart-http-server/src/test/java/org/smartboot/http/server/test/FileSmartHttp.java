9b9bb625ed0c68e683bc5e3bff3ab0623abb4087
三刀 <junwei.zheng@hipac.cn>
2019-11-07 21:36:20 +0800

    更新代码

--- smart-http-server/src/test/java/org/smartboot/http/server/test/FileSmartHttp.java
+++ smart-http-server/src/test/java/org/smartboot/http/server/test/FileSmartHttp.java
@@ -1,10 +1,11 @@
 package org.smartboot.http.server.test;
 
 import org.smartboot.http.HttpBootstrap;
-import org.smartboot.http.server.handle.RouteHandle;
+import org.smartboot.http.server.handle.StaticResourceHandle;
 
 /**
  * 打开浏览器请求：http://127.0.0.0:8080/
+ *
  * @author 三刀
  * @version V1.0 , 2019/11/3
  */
@@ -13,7 +14,7 @@ public class FileSmartHttp {
         String webdir = System.getProperty("user.dir") + "/smart-http-server/webapps";
         HttpBootstrap bootstrap = new HttpBootstrap();
         //配置HTTP消息处理管道
-        bootstrap.pipeline().next(new RouteHandle()).;
+        bootstrap.pipeline().next(new StaticResourceHandle(webdir));
 
         //设定服务器配置并启动
         bootstrap.setPort(8080).start();
