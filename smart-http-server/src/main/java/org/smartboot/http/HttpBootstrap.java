0427691ff74dac8a359619b4ee978d46977b8113
三刀 <junwei.zheng@hipac.cn>
2020-11-17 20:57:52 +0800

    升级正式包

--- smart-http-server/src/main/java/org/smartboot/http/HttpBootstrap.java
+++ smart-http-server/src/main/java/org/smartboot/http/HttpBootstrap.java
@@ -29,7 +29,7 @@ public class HttpBootstrap {
             "                                                       | |    \n" +
             "                                                       (_)   ";
 
-    private static final String VERSION = "1.0.7";
+    private static final String VERSION = "1.0.8";
     private AioQuickServer<Request> server;
 
     /**
