e05411ee8d79b741872bb2b1103aa7dc11c36c48
三刀 <zhengjunweimail@163.com>
2021-03-05 09:11:22 +0800

    优化Attachment设计

--- smart-http-common/src/main/java/org/smartboot/http/common/utils/AttachKey.java
+++ smart-http-common/src/main/java/org/smartboot/http/common/utils/AttachKey.java
@@ -10,7 +10,6 @@ package org.smartboot.http.common.utils;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author 三刀
@@ -18,39 +17,23 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public final class AttachKey<T> {
     /**
-     * 支持附件数量上限
-     */
-    public static final int MAX_ATTACHE_COUNT = 128;
-    /**
      * 缓存同名Key
      */
-    private static final ConcurrentMap<String, AttachKey> NAMES = new ConcurrentHashMap<>();
-    /**
-     * 索引构造器
-     */
-    private static final AtomicInteger INDEX_BUILDER = new AtomicInteger(0);
+    private static final ConcurrentMap<String, AttachKey> KEY_CACHE = new ConcurrentHashMap<>();
     /**
      * 附件名称
      */
     private final String key;
-    /**
-     * 附件索引
-     */
-    private final int index;
 
     private AttachKey(String key) {
         this.key = key;
-        this.index = INDEX_BUILDER.getAndIncrement();
-        if (this.index < 0 || this.index >= MAX_ATTACHE_COUNT) {
-            throw new RuntimeException("too many attach key");
-        }
     }
 
     public static <T> AttachKey<T> valueOf(String name) {
-        AttachKey<T> option = NAMES.get(name);
+        AttachKey<T> option = KEY_CACHE.get(name);
         if (option == null) {
-            option = new AttachKey<T>(name);
-            AttachKey<T> old = NAMES.putIfAbsent(name, option);
+            option = new AttachKey<>(name);
+            AttachKey<T> old = KEY_CACHE.putIfAbsent(name, option);
             if (old != null) {
                 option = old;
             }
@@ -60,10 +43,5 @@ public final class AttachKey<T> {
 
     public String getKey() {
         return key;
-    }
-
-
-    int getIndex() {
-        return index;
     }
 }
