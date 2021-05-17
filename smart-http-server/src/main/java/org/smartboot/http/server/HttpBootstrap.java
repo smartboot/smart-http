0b1a695e0ab5069a7022b6c68c455c6a441a4858
三刀 <zhengjunweimail@163.com>
2021-05-17 09:55:40 +0800

    正式包 1.1.4

--- smart-http-server/src/main/java/org/smartboot/http/server/HttpBootstrap.java
+++ smart-http-server/src/main/java/org/smartboot/http/server/HttpBootstrap.java
@@ -27,7 +27,7 @@ public class HttpBootstrap {
             "                                                       | |    \n" +
             "                                                       (_)   ";
 
-    private static final String VERSION = "1.1.4-SNAPSHOT";
+    private static final String VERSION = "1.1.4";
     /**
      * http消息解码器
      */
