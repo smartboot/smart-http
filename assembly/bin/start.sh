6308b519e223770093f3f931ead248c7c60770d9
三刀 <zhengjunweimail@163.com>
2021-06-20 09:52:10 +0800

    移除assembly模块

--- assembly/bin/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-source setEnv.sh
-java ${JAVA_OPTS} org.smartboot.http.BootStrap
---
+++ smart-http-test/src/main/java/org/smartboot/http/demo/FileSmartHttp.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2017-2021, org.smartboot. All rights reserved.
+ * project name: smart-http
+ * file name: FileSmartHttp.java
+ * Date: 2021-06-20
+ * Author: sandao (zhengjunweimail@163.com)
+ ******************************************************************************/
+
+package org.smartboot.http.demo;
+
+import org.smartboot.http.server.HttpBootstrap;
+import org.smartboot.http.server.handler.HttpStaticResourceHandler;
+
+/**
+ * 打开浏览器请求：http://127.0.0.0:8080/
+ *
+ * @author 三刀
+ * @version V1.0 , 2019/11/3
+ */
+public class FileSmartHttp {
+    public static void main(String[] args) {
+        String webdir = System.getProperty("user.dir") + "/assembly/webapps";
+        HttpBootstrap bootstrap = new HttpBootstrap();
+        //配置HTTP消息处理管道
+        bootstrap.pipeline().next(new HttpStaticResourceHandler(webdir));
+
+        //设定服务器配置并启动
+        bootstrap.start();
+    }
+}
