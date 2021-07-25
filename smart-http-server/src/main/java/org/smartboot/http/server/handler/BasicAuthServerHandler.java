57774a626307747e75aa8ddc2c916f5df3659ad3
三刀 <zhengjunweimail@163.com>
2021-07-25 09:24:49 +0800

    支持http代理

--- smart-http-server/src/main/java/org/smartboot/http/server/handler/BasicAuthServerHandler.java
+++ smart-http-server/src/main/java/org/smartboot/http/server/handler/BasicAuthServerHandler.java
@@ -14,8 +14,10 @@ import org.smartboot.http.common.utils.StringUtils;
 import org.smartboot.http.server.HttpRequest;
 import org.smartboot.http.server.HttpResponse;
 import org.smartboot.http.server.HttpServerHandler;
+import org.smartboot.http.server.impl.Request;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Base64;
 
 /**
@@ -32,14 +34,31 @@ public final class BasicAuthServerHandler extends HttpServerHandler {
     }
 
     @Override
-    public void handle(HttpRequest request, HttpResponse response) throws IOException {
+    public void onHeaderComplete(Request request) throws IOException {
         String clientBasic = request.getHeader(HeaderNameEnum.AUTHORIZATION.getName());
         if (StringUtils.equals(clientBasic, this.basic)) {
-            httpServerHandler.handle(request, response);
+            httpServerHandler.onHeaderComplete(request);
         } else {
+            HttpResponse response = request.newHttpRequest().getResponse();
             response.setHeader(HeaderNameEnum.WWW_AUTHENTICATE.getName(), "Basic realm=\"smart-http\"");
             response.setHttpStatus(HttpStatus.UNAUTHORIZED);
+            response.close();
         }
+    }
+
+    @Override
+    public boolean onBodyStream(ByteBuffer buffer, Request request) {
+        return httpServerHandler.onBodyStream(buffer, request);
+    }
+
+    @Override
+    public void onClose(Request request) {
+        httpServerHandler.onClose(request);
+    }
+
+    @Override
+    public void handle(HttpRequest request, HttpResponse response) throws IOException {
+        httpServerHandler.handle(request, response);
     }
 
 }
