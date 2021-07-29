4f1b29c20428ea606b5fb5a8b344aa5157a8628a
三刀 <zhengjunweimail@163.com>
2021-07-29 13:38:22 +0800

    1.1.8-SNAPSHOT

--- smart-http-server/src/main/java/org/smartboot/http/server/HttpBootstrap.java
+++ smart-http-server/src/main/java/org/smartboot/http/server/HttpBootstrap.java
@@ -27,7 +27,7 @@ public class HttpBootstrap {
             "                                                       | |    \n" +
             "                                                       (_)   ";
 
-    private static final String VERSION = "1.1.7";
+    private static final String VERSION = "1.1.8-SNAPSHOT";
     /**
      * http消息解码器
      */
