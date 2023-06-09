43213ca569adb15bd18dccb3d385e5f897499b6b
三刀 <zhengjunweimail@163.com>
2023-06-09 23:14:12 +0800

    1.2.3

--- smart-http-server/src/main/java/org/smartboot/http/server/HttpBootstrap.java
+++ smart-http-server/src/main/java/org/smartboot/http/server/HttpBootstrap.java
@@ -31,7 +31,7 @@ public class HttpBootstrap {
             "                                                       | |    \n" +
             "                                                       (_)   ";
 
-    private static final String VERSION = "1.2.3-SNAPSHOT";
+    private static final String VERSION = "1.2.3";
     /**
      * http消息解码器
      */
