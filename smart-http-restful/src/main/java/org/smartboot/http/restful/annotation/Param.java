25ddf1a7a16cadd69eadc6f7e074acdd60fb73f1
三刀 <zhengjunweimail@163.com>
2023-08-28 18:59:26 +0800

    1.3.0

--- smart-http-restful/src/main/java/org/smartboot/http/restful/annotation/Param.java
+++ smart-http-restful/src/main/java/org/smartboot/http/restful/annotation/Param.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 /**
  * @author qinluo
  * @date 2023-07-07 14:49:11
- * @since 1.3.0-SNAPSHOT
+ * @since 1.3.0
  */
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
