b357ff38601f01037d19ec131686cb378793f650
zhengjw22 <zhengjunweimail@163.com>
2023-12-26 18:55:17 +0800

    1.3.7-SNAPSHOT

---
+++ smart-http-restful/src/main/java/org/smartboot/http/restful/annotation/CacheKey.java
@@ -0,0 +1,15 @@
+package org.smartboot.http.restful.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target(PARAMETER)
+public @interface CacheKey {
+    String value();
+}
