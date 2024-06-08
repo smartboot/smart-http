42f78ab91187dd6a4eceeaa98d48fa9e831e7ee9
zhengjw22 <zhengjunweimail@163.com>
2024-06-08 10:20:08 +0800

    1.4.3-SNAPSHOT

--- smart-http-server/src/main/java/org/smartboot/http/server/HttpServerConfiguration.java
+++ smart-http-server/src/main/java/org/smartboot/http/server/HttpServerConfiguration.java
@@ -28,7 +28,7 @@ import java.util.function.Function;
  * @version V1.0 , 2021/2/22
  */
 public class HttpServerConfiguration {
-    public static final String VERSION = "1.4.2";
+    public static final String VERSION = "1.4.3-SNAPSHOT";
 
     /**
      * 缓存
