25785f1f33ad4dfd4a318437e108312b8ab1e6b9
zhengjw22 <zhengjunweimail@163.com>
2024-06-20 18:51:05 +0800

    1.5.1

--- smart-http-server/src/main/java/org/smartboot/http/server/HttpServerConfiguration.java
+++ smart-http-server/src/main/java/org/smartboot/http/server/HttpServerConfiguration.java
@@ -28,7 +28,7 @@ import java.util.function.Function;
  * @version V1.0 , 2021/2/22
  */
 public class HttpServerConfiguration {
-    public static final String VERSION = "1.5.1-SNAPSHOT";
+    public static final String VERSION = "1.5.1";
 
     /**
      * 缓存
