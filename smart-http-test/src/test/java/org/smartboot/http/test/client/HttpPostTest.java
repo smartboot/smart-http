8561939810b7e06b891fa6e7f4899e409f1dd353
zhengjw22 <zhengjunweimail@163.com>
2024-07-12 13:12:10 +0800

    补充https的单元测试

--- smart-http-test/src/test/java/org/smartboot/http/test/client/HttpPostTest.java
+++ smart-http-test/src/test/java/org/smartboot/http/test/client/HttpPostTest.java
@@ -24,6 +24,9 @@ import org.smartboot.http.server.HttpResponse;
 import org.smartboot.http.server.HttpServerHandler;
 import org.smartboot.http.server.handler.HttpRouteHandler;
 import org.smartboot.http.server.impl.Request;
+import org.smartboot.socket.extension.plugins.SslPlugin;
+import org.smartboot.socket.extension.ssl.ClientAuth;
+import org.smartboot.socket.extension.ssl.factory.ServerSSLContextFactory;
 import org.smartboot.socket.util.AttachKey;
 import org.smartboot.socket.util.Attachment;
 
@@ -35,7 +38,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
@@ -45,10 +47,11 @@ import java.util.concurrent.Future;
 public class HttpPostTest {
 
     private HttpBootstrap httpBootstrap;
+    private HttpBootstrap httpsBootstrap;
 
     @Before
-    public void init() {
-        httpBootstrap = new HttpBootstrap();
+    public void init() throws Exception {
+
         HttpRouteHandler routeHandle = new HttpRouteHandler();
         routeHandle.route("/post_param", new HttpServerHandler() {
             @Override
@@ -137,125 +140,154 @@ public class HttpPostTest {
             }
         });
 
+        httpBootstrap = new HttpBootstrap();
         httpBootstrap.httpHandler(routeHandle).setPort(8080).start();
+
+        SslPlugin sslPlugin = new SslPlugin(new ServerSSLContextFactory(HttpPostTest.class.getClassLoader().getResourceAsStream("server.keystore"), "123456", "123456"), ClientAuth.NONE);
+        httpsBootstrap = new HttpBootstrap();
+        httpsBootstrap.configuration().addPlugin(sslPlugin);
+        httpsBootstrap.httpHandler(routeHandle).setPort(8888).start();
     }
 
     @Test
-    public void testChunkedRequest() throws ExecutionException, InterruptedException {
-        HttpClient client = new HttpClient("127.0.0.1", 8080);
-        String body = "test a body string";
-        client.configuration().debug(true);
-        Future<org.smartboot.http.client.HttpResponse> future = client.post("/chunk")
-                .header().keepalive(true).done()
-                .body()
-                .write(body.getBytes()).write(body.getBytes())
-                .done()
-                .onSuccess(response -> {
-                    System.out.println(response.body());
-                })
-                .onFailure(t -> {
-                    System.out.println(t.getMessage());
-                }).done();
-//        JSONObject jsonObject = JSONObject.parseObject(future.get().body());
-        Assert.assertEquals(future.get().body(), body + body);
+    public void testChunkedRequest() throws Throwable {
+
+        Consumer consumer = client -> {
+            String body = "test a body string";
+            client.configuration().debug(true);
+            Future<org.smartboot.http.client.HttpResponse> future = client.post("/chunk")
+                    .header().keepalive(true).done()
+                    .body()
+                    .write(body.getBytes()).write(body.getBytes())
+                    .done()
+                    .onSuccess(response -> {
+                        System.out.println(response.body());
+                    })
+                    .onFailure(t -> {
+                        System.out.println(t.getMessage());
+                    }).done();
+            Assert.assertEquals(future.get().body(), body + body);
+        };
+        doRequest(new HttpClient("http://127.0.0.1:8080"), consumer);
+        doRequest(new HttpClient("https://127.0.0.1:8888"), consumer);
+    }
+
+    interface Consumer {
+        void accept(HttpClient httpClient) throws Throwable;
+    }
+
+    private void doRequest(HttpClient client, Consumer consumer) throws Throwable {
+        consumer.accept(client);
     }
 
     @Test
-    public void testCheckHeader() throws InterruptedException, ExecutionException {
-        HttpClient client = new HttpClient("127.0.0.1", 8080);
-        String body = "test a body string";
-        client.configuration().debug(true);
-        Future<org.smartboot.http.client.HttpResponse> future = client.post("/header")
-                .header().keepalive(true).setContentLength(body.getBytes().length).done()
-                .body()
-                .write(body.getBytes())
-                .done()
-                .onSuccess(response -> {
-                    System.out.println(response.body());
-                })
-                .onFailure(t -> {
-                    System.out.println(t.getMessage());
-                }).done();
-        JSONObject jsonObject = JSONObject.parseObject(future.get().body());
-        Assert.assertNull(jsonObject.getString(HeaderNameEnum.TRANSFER_ENCODING.getName()));
-        Assert.assertEquals(jsonObject.getString(HeaderNameEnum.CONTENT_LENGTH.getName()), String.valueOf(body.getBytes().length));
+    public void testCheckHeader() throws Throwable {
+        Consumer consumer = client -> {
+            String body = "test a body string";
+            client.configuration().debug(true);
+            Future<org.smartboot.http.client.HttpResponse> future = client.post("/header")
+                    .header().keepalive(true).setContentLength(body.getBytes().length).done()
+                    .body()
+                    .write(body.getBytes())
+                    .done()
+                    .onSuccess(response -> {
+                        System.out.println(response.body());
+                    })
+                    .onFailure(t -> {
+                        System.out.println(t.getMessage());
+                    }).done();
+            JSONObject jsonObject = JSONObject.parseObject(future.get().body());
+            Assert.assertNull(jsonObject.getString(HeaderNameEnum.TRANSFER_ENCODING.getName()));
+            Assert.assertEquals(jsonObject.getString(HeaderNameEnum.CONTENT_LENGTH.getName()), String.valueOf(body.getBytes().length));
+        };
+        doRequest(new HttpClient("http://127.0.0.1:8080"), consumer);
+        doRequest(new HttpClient("https://127.0.0.1:8888"), consumer);
     }
 
     @Test
-    public void testChunked() throws InterruptedException, ExecutionException {
-        HttpClient client = new HttpClient("127.0.0.1", 8080);
-        String body = "test a body string";
-        String body2 = "test a body2 string";
-        client.configuration().debug(true);
-        Future<org.smartboot.http.client.HttpResponse> future1 = client.post("/other/abc")
-                .header().keepalive(true).setContentLength(body.getBytes().length).done()
-                .body()
-                .write(body.getBytes())
-                .done()
-                .onSuccess(response -> {
-                    System.out.println(response.body());
-                    System.out.println("1111");
-                })
-                .onFailure(t -> {
-                    System.out.println(t.getMessage());
-                }).done();
+    public void testChunked() throws Throwable {
+        Consumer consumer = client -> {
+            String body = "test a body string";
+            String body2 = "test a body2 string";
+            client.configuration().debug(true);
+            Future<org.smartboot.http.client.HttpResponse> future1 = client.post("/other/abc")
+                    .header().keepalive(true).setContentLength(body.getBytes().length).done()
+                    .body()
+                    .write(body.getBytes())
+                    .done()
+                    .onSuccess(response -> {
+                        System.out.println(response.body());
+                        System.out.println("1111");
+                    })
+                    .onFailure(t -> {
+                        System.out.println(t.getMessage());
+                    }).done();
 
-        Future<org.smartboot.http.client.HttpResponse> future2 = client.post("/other/abc")
-                .header().keepalive(true).setContentLength(body2.getBytes().length).done()
-                .body()
-                .write(body2.getBytes())
-                .done()
-                .onSuccess(response -> {
-                    System.out.println(response.body());
-                    System.out.println("222");
-                })
-                .onFailure(t -> {
-                    System.out.println(t.getMessage());
-                }).done();
-        Assert.assertEquals(body, future1.get().body());
-        Assert.assertEquals(body2, future2.get().body());
+            Future<org.smartboot.http.client.HttpResponse> future2 = client.post("/other/abc")
+                    .header().keepalive(true).setContentLength(body2.getBytes().length).done()
+                    .body()
+                    .write(body2.getBytes())
+                    .done()
+                    .onSuccess(response -> {
+                        System.out.println(response.body());
+                        System.out.println("222");
+                    })
+                    .onFailure(t -> {
+                        System.out.println(t.getMessage());
+                    }).done();
+            Assert.assertEquals(body, future1.get().body());
+            Assert.assertEquals(body2, future2.get().body());
+        };
+
     }
 
     @Test
-    public void testPost() throws ExecutionException, InterruptedException {
-        CompletableFuture<Boolean> future = new CompletableFuture<>();
-        HttpClient httpClient = new HttpClient("localhost", 8080);
-        httpClient.configuration().debug(true);
-        Map<String, String> param = new HashMap<>();
-        param.put("name", "zhouyu");
-        param.put("age", "18");
-        httpClient.post("/post_param").header().setContentType(HeaderValueEnum.X_WWW_FORM_URLENCODED.getName()).done().onSuccess(response -> {
-            System.out.println(response.body());
-            JSONObject jsonObject = JSONObject.parseObject(response.body());
-            boolean suc = false;
-            for (String key : param.keySet()) {
-                suc = StringUtils.equals(param.get(key), jsonObject.getString(key));
-                if (!suc) {
-                    break;
+    public void testPost() throws Throwable {
+        Consumer consumer = httpClient -> {
+            CompletableFuture<Boolean> future = new CompletableFuture<>();
+            httpClient.configuration().debug(true);
+            Map<String, String> param = new HashMap<>();
+            param.put("name", "zhouyu");
+            param.put("age", "18");
+            httpClient.post("/post_param").header().setContentType(HeaderValueEnum.X_WWW_FORM_URLENCODED.getName()).done().onSuccess(response -> {
+                System.out.println(response.body());
+                JSONObject jsonObject = JSONObject.parseObject(response.body());
+                boolean suc = false;
+                for (String key : param.keySet()) {
+                    suc = StringUtils.equals(param.get(key), jsonObject.getString(key));
+                    if (!suc) {
+                        break;
+                    }
                 }
-            }
-            httpClient.close();
-            future.complete(suc);
-        }).onFailure(throwable -> {
-            System.out.println("异常A: " + throwable.getMessage());
-            throwable.printStackTrace();
-            Assert.fail();
-            future.complete(false);
-        }).body().formUrlencoded(param);
-        Assert.assertTrue(future.get());
+                httpClient.close();
+                future.complete(suc);
+            }).onFailure(throwable -> {
+                System.out.println("异常A: " + throwable.getMessage());
+                throwable.printStackTrace();
+                Assert.fail();
+                future.complete(false);
+            }).body().formUrlencoded(param);
+            Assert.assertTrue(future.get());
+        };
+        doRequest(new HttpClient("http://127.0.0.1:8080"), consumer);
+        doRequest(new HttpClient("https://127.0.0.1:8888"), consumer);
     }
 
     @Test
-    public void testJson() throws InterruptedException {
-        HttpClient httpClient = new HttpClient("localhost", 8080);
-        httpClient.configuration().debug(true);
-        byte[] jsonBytes = "{\"a\":1,\"b\":\"123\"}".getBytes(StandardCharsets.UTF_8);
-        httpClient.post("/json").header().setContentLength(jsonBytes.length).setContentType("application/json").done().body().write(jsonBytes).flush().done();
-        Thread.sleep(100);
+    public void testJson() throws Throwable {
+        Consumer consumer = httpClient -> {
+            httpClient.configuration().debug(true);
+            byte[] jsonBytes = "{\"a\":1,\"b\":\"123\"}".getBytes(StandardCharsets.UTF_8);
+            httpClient.post("/json").header().setContentLength(jsonBytes.length).setContentType("application/json").done().body().write(jsonBytes).flush().done();
+            Thread.sleep(100);
+        };
+        doRequest(new HttpClient("http://127.0.0.1:8080"), consumer);
+        doRequest(new HttpClient("https://127.0.0.1:8888"), consumer);
     }
 
     @After
     public void destroy() {
         httpBootstrap.shutdown();
+        httpsBootstrap.shutdown();
     }
 }
