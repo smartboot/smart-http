275258c7d01f8f54bac742a0b4d5c37c901b70df
zhengjw22 <zhengjunweimail@163.com>
2024-11-29 20:46:19 +0800

    2.2发布

--- smart-http-server/src/main/java/org/smartboot/http/server/HttpServerConfiguration.java
+++ smart-http-server/src/main/java/org/smartboot/http/server/HttpServerConfiguration.java
@@ -29,7 +29,7 @@ import java.util.List;
  * @version V1.0 , 2021/2/22
  */
 public class HttpServerConfiguration {
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "2.2";
 
     /**
      * 缓存
